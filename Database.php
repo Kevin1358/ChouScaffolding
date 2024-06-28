2ba968757181a43533bb938595b874bcc73be463
Kevin1358 <tembok511@gmail.com>
2024-06-28 21:17:37 +0700

    Implement Generalize Database

---
+++ Database.php
@@ -0,0 +1,24 @@
+<?php
+class Database{
+    public mysqli $connection;
+    public function __construct(string $host,string $username,string $password,string $database) {
+        $this->connection = new mysqli($host,$username,$password,$database);
+    }
+    public function transaction($handler){
+        $this->connection->begin_transaction();
+        try {
+            $handler($this->connection);
+            $this->connection->commit();
+        } catch (\Throwable $th) {
+            $this->connection->rollback();
+            throw $th;
+        }
+    }
+    public function query(string $query,string $type,array $variable){
+        $statement = $this->connection->prepare($query);
+        $statement->bind_param($type,...$variable);
+        $statement->execute();
+        return $statement->get_result();
+    }
+}
+?>
