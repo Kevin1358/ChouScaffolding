db69e0ba476581d2d443bdec3c63f8f5890382fe
Joseph Chou Jyh Yeong <joseph.cjy@ad-ins.com>
2024-12-13 16:14:35 +0700

    implement namespace

--- MessageQueue.php
+++ MessageQueue.php
@@ -1,4 +1,5 @@
 <?php
+namespace ChouScaffolding\MessageQueue;
 class MessageQueueProducer extends MessageQueueBase{
     public function Produce(string $message):void{
         $insertQuery = "INSERT INTO `$this->Table`(`message`, `status`) VALUES (?,?)";
@@ -16,27 +17,27 @@ class MessageQueueConsumer extends MessageQueueBase{
             if($selectStmt->rowCount() == 0){
                 throw new NoMesssageException();
             }
-            $selectResult = $selectStmt->fetch(PDO::FETCH_ASSOC);
+            $selectResult = $selectStmt->fetch(\PDO::FETCH_ASSOC);
             $this->UpdateStatus($selectResult["message"],MessageQueueBase::$STATUS_CONSUMED);
             $this->Db->commit();
             return $selectResult;
         }catch(MessageQueueException $mex){
             $this->Db->rollBack();
-        }catch(Throwable $e){
+        }catch(\Throwable $e){
             $this->Db->rollBack();
             throw $e;
         }
     }
 }
 interface MessageQueueException{}
-class NoMesssageException extends Exception implements MessageQueueException{}
+class NoMesssageException extends \Exception implements MessageQueueException{}
 class MessageQueueBase{
     static string $STATUS_INIT = 'INIT';
     static string $STATUS_CONSUMED = 'CONSUMED';
     static string $STATUS_PUKED = 'PUKED';
-    protected PDO $Db;
+    protected \PDO $Db;
     protected string $Table;
-    public function __construct(PDO $db, string $table) {
+    public function __construct(\PDO $db, string $table) {
         $this->Db = $db;
         $this->Table = $table;
     }
